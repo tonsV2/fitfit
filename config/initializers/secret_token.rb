b71f9ba74d2c40cb83a615c2ca3a3aa6d180cdbe
Super Snot <snot@rails3.(none)>
2011-02-08 17:49:41 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitfit::Application.config.secret_token = '7b460dbbea850efee013bbc60ab1d3add201310eb9666f60be24a96b2269182ec55147676e4732557fb4cba934000565865d5f0d1d11d7ac96d3abf591e98170'
